f4e65a1c594b149e7379ee125559f9f2796b9b62
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2022-01-26 17:54:33 +0200

    init;task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let numbStr = '' + n;
+  let ArrStr = numbStr.split('');
+  console.log(ArrStr)
+  let newArr = [];
+  for (let i=0; i<ArrStr.length; i++) {
+    newArr[i] = ArrStr[ArrStr.length-1-i]
+  }
+  console.log(newArr)
+  newArr = parseInt(newArr.join(''));
+  console.log(newArr)
+  return newArr
 }
